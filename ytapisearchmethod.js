d78a843d61c6469a16c09bf5c74a8d3a558b5061
Aryan Pandey <aryanpandey1507@gmail.com>
2021-06-13 00:12:06 +0530

    Add files via upload

---
+++ ytapisearchmethod.js
@@ -0,0 +1,10 @@
+// firstly i went to console.google.developer
+// then i created a new project namely music bot
+// then i went to the dashboard
+// then i went to api library
+// then i searched for the "yt-data-api v3"
+// then i enabled it
+// then it opened and i went to credentials
+// then go to create credentials 
+// then create API key
+// you will get your api key
